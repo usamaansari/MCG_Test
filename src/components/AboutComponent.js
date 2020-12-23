00ebe7d68319187cb821a3ee2069d6317cfc77ce
usama <usamazafaransari@gmail.com>
2020-12-22 21:27:24 -0800

    Update in MCG_Test

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -43,13 +43,11 @@ function AboutComponent({
                 >
                   {description}
                 </p>
-                <div className='home_button'>
                 <Link to='/sign-up'>
                   <Button buttonSize='btn--wide' buttonColor='blue'>
                     {buttonLabel}
                   </Button>
                 </Link>
-                </div>
               </div>
             </div>
             <div className='col'>
